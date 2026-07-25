5bb2113d1f3ea667a3954c3860c51e4b8f44ef6d
Ryan Mills <rtmills09@gmail.com>
2026-07-25 18:03:19 -0500

    Title fade

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,11 +1,33 @@
+import { useState, useRef, useEffect } from "react";
+
 interface TitleProps {
     text: string;
 }
 
 function Title({ text }: TitleProps) {
+    const [displayText, setDisplayText] = useState(text);
+    const [isFading, setIsFading] = useState(false);
+    const prevTextRef = useRef(text);
+
+    useEffect(() => {
+        // Control fading
+        if (prevTextRef.current === text) {
+            return;
+        }
+ 
+        setIsFading(true);
+
+        window.setTimeout(() => {
+            setDisplayText(text);
+            setIsFading(false);
+            prevTextRef.current = text;
+        }, 300); 
+    }, [text]);
+
+
     return (
-        <h1 id="title" className='title'>
-            {text}
+        <h1 id="title" className={`title ${isFading && 'fade-out'}`}>
+            {displayText}
         </h1>
     );
 }
