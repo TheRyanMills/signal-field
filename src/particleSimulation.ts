15be15236c8828e20cc7d0ee5abc4b80411f007f
Ryan Mills <rtmills09@gmail.com>
2026-07-25 16:31:36 -0500

    Added Speed and Acceleration values to Config

--- src/particleSimulation.ts
+++ src/particleSimulation.ts
@@ -4,6 +4,14 @@ import * as THREE from 'three';
 const CLOUD_RADIUS = 1;
 const PARTICLE_COUNT = 10000;
 
+const ROT_SPEED_START_X = 0.1;
+const ROT_SPEED_START_Y = 0.2;
+
+const ROT_SPEED_ACCL = 0.5;
+const ROT_SPEED_TARGET_X = 0.2;
+const ROT_SPEED_TARGET_Y = 0.3;
+
+
 // Scene Set-up
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -12,13 +20,16 @@ camera.position.z = 10;
 let pointCloud = new THREE.Points();
 let renderer = new THREE.WebGLRenderer();
 
+
 // Handle Resize
 window.addEventListener('resize', () => {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
+
+// Driver Logic
 export function renderParticleSimulation() {
     pointCloud = createPointCloud();
 
@@ -35,55 +46,66 @@ export function renderParticleSimulation() {
 }
 
 function createPointCloud(): THREE.Points {
-  const maxRadius = CLOUD_RADIUS * 5;
-  const positions = new Float32Array(PARTICLE_COUNT * 3);
-  const colors = new Float32Array(PARTICLE_COUNT * 3);
-  const color = new THREE.Color();
+    const maxRadius = CLOUD_RADIUS * 5;
+    const positions = new Float32Array(PARTICLE_COUNT * 3);
+    const colors = new Float32Array(PARTICLE_COUNT * 3);
+    const color = new THREE.Color();
 
-  for (let i = 0; i < PARTICLE_COUNT; i++) {
-    // Initialize positions - distribute points within a sphere
-    const r = maxRadius * Math.cbrt(Math.random());
-    const theta = Math.random() * Math.PI * 2;
-    const phi = Math.acos(2 * Math.random() - 1);
+    for (let i = 0; i < PARTICLE_COUNT; i++) {
+        // Initialize positions - distribute points within a sphere
+        const r = maxRadius * Math.cbrt(Math.random());
+        const theta = Math.random() * Math.PI * 2;
+        const phi = Math.acos(2 * Math.random() - 1);
 
-    const x = r * Math.sin(phi) * Math.cos(theta);
-    const y = r * Math.sin(phi) * Math.sin(theta);
-    const z = r * Math.cos(phi);
+        const x = r * Math.sin(phi) * Math.cos(theta);
+        const y = r * Math.sin(phi) * Math.sin(theta);
+        const z = r * Math.cos(phi);
 
-    positions[i * 3] = x;
-    positions[i * 3 + 1] = y;
-    positions[i * 3 + 2] = z;
+        positions[i * 3] = x;
+        positions[i * 3 + 1] = y;
+        positions[i * 3 + 2] = z;
 
-    // Initialize colors - scale hue based on distance from center
-    const dist = r / maxRadius;
+        // Initialize colors - scale hue based on distance from center
+        const dist = r / maxRadius;
 
-    color.setHSL(1.0 - dist, 0.8, 0.5);
+        color.setHSL(1.0 - dist, 0.8, 0.5);
 
-    colors[i * 3] = color.r;
-    colors[i * 3 + 1] = color.g;
-    colors[i * 3 + 2] = color.b;
-  }
+        colors[i * 3] = color.r;
+        colors[i * 3 + 1] = color.g;
+        colors[i * 3 + 2] = color.b;
+    }
 
-  const geometry = new THREE.BufferGeometry();
-  geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
-  geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+    geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
 
-  const material = new THREE.PointsMaterial({
-    size: 0.05,
-    vertexColors: true,
-    transparent: true,
-    opacity: 0.9,
-    depthWrite: false,
-    blending: THREE.AdditiveBlending,
-    sizeAttenuation: true,
-  });
+    const material = new THREE.PointsMaterial({
+        size: 0.05,
+        vertexColors: true,
+        transparent: true,
+        opacity: 0.9,
+        depthWrite: false,
+        blending: THREE.AdditiveBlending,
+        sizeAttenuation: true,
+    });
 
-  return new THREE.Points(geometry, material);
+    return new THREE.Points(geometry, material);
 }
 
+
+// Util Functions
 function animate(time: number) {
-    pointCloud.rotation.x = time / 2000;
-    pointCloud.rotation.y = time / 1000;
+    const secsElapsed = time / 1000;
+
+    // Apply rotation speed
+    pointCloud.rotation.x = secsElapsed * getVelocity(ROT_SPEED_ACCL, ROT_SPEED_START_X, ROT_SPEED_TARGET_X, secsElapsed);
+    pointCloud.rotation.y = secsElapsed * getVelocity(ROT_SPEED_ACCL, ROT_SPEED_START_Y, ROT_SPEED_TARGET_Y, secsElapsed);
 
     renderer.render(scene, camera);
+}
+
+function getVelocity(accel: number, startSpeed: number, targetSpeed: number, secsElapsed: number): number {
+    // Return rotation velocity at given time as it converges to the target value
+    const decay = Math.exp(-1.0 * Math.abs(accel) * secsElapsed);
+    return targetSpeed + (startSpeed - targetSpeed) * decay;
 }
